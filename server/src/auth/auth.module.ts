c43a3f4b54431fcd6c89c2b469d5e822cbe21748
maximus <badman7755995@gmail.com>
2023-02-09 17:37:55 +0300

    add database

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { Services } from '../utils/constants';
-import { UserModule } from '../users/user.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [
     {
