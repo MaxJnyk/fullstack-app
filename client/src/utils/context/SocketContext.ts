8d02868cbd7a9cf7e2f5a672f6532a16c957e3e1
MaxJnyk <badman7755995@gmail.com>
2023-04-15 11:55:36 -0400

    Frontend: add withCredentials for socket io connection

--- client/src/utils/context/SocketContext.ts
+++ client/src/utils/context/SocketContext.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 import { createContext } from "react";
 
-export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!);
+export const socket = io(process.env.REACT_APP_WEBSOCKET_URL!, {
+  withCredentials: true,
+});
 
 export const SocketContext = createContext(socket);
