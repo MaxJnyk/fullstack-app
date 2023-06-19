d7b9a736abcb0f7f079d4c676d667a9d3e4aa9b9
MaxJnyk <badman7755995@gmail.com>
2023-06-19 08:31:13 -0400

    fix --move socket event listen to conversationPage

--- client/src/pages/ConversationChannelPage.tsx
+++ client/src/pages/ConversationChannelPage.tsx
@@ -1,15 +1,15 @@
-import { useContext, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { MessagePanel } from '../components/messages/MessagePanel';
-import { getConversationMessages } from '../utils/api';
-import { AuthContext } from '../utils/context/AuthContext';
-import { SocketContext } from '../utils/context/SocketContext';
-import { ConversationChannelPageStyle } from '../utils/styles';
-import { MessageEventPayload, MessageType } from '../utils/types';
-import { AppDispatch, RootState } from '../store';
-import { addMessage, fetchMessagesThunk } from '../store/messageSlice';
-import { updateConversation } from '../store/conversationSlice';
+import { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { MessagePanel } from "../components/messages/MessagePanel";
+import { getConversationMessages } from "../utils/api";
+import { AuthContext } from "../utils/context/AuthContext";
+import { SocketContext } from "../utils/context/SocketContext";
+import { ConversationChannelPageStyle } from "../utils/styles";
+import { MessageEventPayload, MessageType } from "../utils/types";
+import { AppDispatch, RootState } from "../store";
+import { addMessage, fetchMessagesThunk } from "../store/messageSlice";
+import { updateConversation } from "../store/conversationSlice";
 
 export const ConversationChannelPage = () => {
   const { user } = useContext(AuthContext);
@@ -23,25 +23,29 @@ export const ConversationChannelPage = () => {
   }, [id]);
 
   useEffect(() => {
-    socket.emit('onClientConnect', {
+    socket.emit("onClientConnect", {
       conversationId: parseInt(id!),
     });
-    socket.on('onMessage', (payload: MessageEventPayload) => {
-      console.log('Message Received');
+    socket.on("connected", (data) => {
+      console.log("Connected to WebSocket");
+      console.log(data);
+    });
+    socket.on("onMessage", (payload: MessageEventPayload) => {
+      console.log("Message Received");
       const { conversation, message } = payload;
       console.log(conversation, message);
       dispatch(addMessage(payload));
       dispatch(updateConversation(conversation));
     });
     return () => {
-      socket.off('connected');
-      socket.off('onMessage');
+      socket.off("connected");
+      socket.off("onMessage");
     };
   }, [id]);
 
   const sendTypingStatus = () => {
-    console.log('You are typing');
-    socket.emit('onUserTyping', { conversationId: id });
+    console.log("You are typing");
+    socket.emit("onUserTyping", { conversationId: id });
   };
 
   return (
--- client/src/pages/ConversationPage.tsx
+++ client/src/pages/ConversationPage.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useParams } from 'react-router-dom';
-import { ConversationPanel } from '../components/conversations/ConversationPanel';
-import { ConversationSidebar } from '../components/conversations/ConversationSidebar';
-import { AppDispatch, RootState } from '../store';
-import { fetchConversationsThunk } from '../store/conversationSlice';
-import { Page } from '../utils/styles';
-import { ConversationType } from '../utils/types';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, useParams } from "react-router-dom";
+import { ConversationPanel } from "../components/conversations/ConversationPanel";
+import { ConversationSidebar } from "../components/conversations/ConversationSidebar";
+import { AppDispatch, RootState } from "../store";
+import { fetchConversationsThunk } from "../store/conversationSlice";
+import { Page } from "../utils/styles";
+import { ConversationType } from "../utils/types";
 
 export const ConversationPage = () => {
   const { id } = useParams();
@@ -18,7 +18,7 @@ export const ConversationPage = () => {
   );
 
   useEffect(() => {
-    console.log('Fetching Conversations in ConversationPage');
+    console.log("Fetching Conversations in ConversationPage");
     console.log(conversationsState.find((c) => c.id === 15));
     dispatch(fetchConversationsThunk());
   }, []);
